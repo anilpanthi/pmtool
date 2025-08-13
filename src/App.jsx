633d5b98315739c3414c107fde13fb6c8b044a75
Anil panthi <apanthi21@gmail.com>
2025-08-13 17:02:31 +0545

    Add project button issue is fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
 import Sidebar from './components/Sidebar';
+import NewProject from './components/NewProject';
+import NoProjectSelected from './components/NoProjectSelected';
+import { useState } from 'react';
 function App() {
-  return (
-    <main className="h-screen my-8 flex gap-8" >
-     <Sidebar />
-    </main>
-  );
+
+    const [projectState, setProjectState] = useState({
+        selectedProjectId: undefined,
+        projects: []
+    });
+    
+    
+    function handleStartAddProject() {
+        setProjectState((prevState) => ({
+            ...prevState,
+            selectedProjectId: null
+        }));
+    }
+    
+    let content ;
+    
+    if(projectState.selectedProjectId === null){
+        content = <NewProject />;
+    }else if(projectState.selectedProjectId === undefined){
+        content = <NoProjectSelected/>
+    }
+
+    return (
+        <main className="h-screen my-8 flex gap-8" >
+            <Sidebar onStartAddProject={handleStartAddProject} />
+            {content}
+        </main>
+    );
 }
 
 export default App;
